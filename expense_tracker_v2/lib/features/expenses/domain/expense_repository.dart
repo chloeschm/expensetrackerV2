02ca2a4a8df5b7b10271fe4f6dfe523a5a647126
chloe <cschmi29@wgu.edu>
2026-03-16 17:40:54 -0500

    creates expense repository impl and updates expense repository to fix delete expense issues

--- expense_tracker_v2/lib/features/expenses/domain/expense_repository.dart
+++ expense_tracker_v2/lib/features/expenses/domain/expense_repository.dart
@@ -1,9 +1,9 @@
 import 'expense.dart';
 
 abstract class ExpenseRepository {
-  Future<Expense?> getExpense(String id);
+  Future<Expense?> getExpense(String id, String tripId);
   Stream<List<Expense>> getExpensesForTrip(String tripId);
   Future<void> addExpense(Expense expense, String tripId);
   Future<void> updateExpense(Expense expense, String tripId);
-  Future<void> deleteExpense(String id, String tripId);
+  Future<void> deleteExpense(String tripId, Expense expense);
 }
---
+++ expense_tracker_v2/lib/features/trips/data/expense_repository_impl.dart
@@ -0,0 +1,98 @@
+import 'package:expense_tracker_v2/features/expenses/domain/expense_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expense_tracker_v2/features/expenses/domain/expense.dart';
+
+class ExpenseRepositoryImpl implements ExpenseRepository {
+  final FirebaseFirestore _firestore;
+  ExpenseRepositoryImpl({FirebaseFirestore? firestore})
+    : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  Expense _expenseFromMap(Map<String, dynamic> map, String documentId) {
+    return Expense(
+      id: documentId,
+      title: map['title'] as String,
+      amount: (map['amount'] as num).toDouble(),
+      date: (map['date'] as Timestamp).toDate(),
+      currency: map['currency'] as String,
+      addedBy: map['addedBy'] as String,
+      category: ExpenseCategory.values.byName(map['category']),
+      notes: map['notes'] as String?,
+    );
+  }
+
+  Map<String, dynamic> _expenseToMap(Expense expense) {
+    return {
+      'id': expense.id,
+      'title': expense.title,
+      'amount': expense.amount,
+      'date': expense.date,
+      'currency': expense.currency,
+      'addedBy': expense.addedBy,
+      'category': expense.category.name,
+      'notes': expense.notes,
+    };
+  }
+
+  @override
+  Future<void> addExpense(Expense expense, String tripId) async {
+    DocumentReference tripRef = _firestore.collection('trips').doc(tripId);
+    await tripRef.update({
+      'expenses': FieldValue.arrayUnion([_expenseToMap(expense)]),
+    });
+  }
+
+  @override
+  Future<void> deleteExpense(String tripId, Expense expense) async {
+    DocumentReference tripRef = _firestore.collection('trips').doc(tripId);
+    await tripRef.update({
+      'expenses': FieldValue.arrayRemove([_expenseToMap(expense)]),
+    });
+  }
+
+  @override
+  Future<void> updateExpense(Expense expense, String tripId) async {
+    final docRef = _firestore.collection('trips').doc(tripId);
+
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(docRef);
+      if (!snapshot.exists) throw Exception("Document does not exist!");
+      List<dynamic> expenses = List.from(snapshot.get('expenses'));
+
+      final updatedExpenses = expenses.map((e) {
+        final map = e as Map<String, dynamic>;
+        if (map['id'] == expense.id) {
+          return _expenseToMap(expense);
+        }
+        return map;
+      }).toList();
+      transaction.update(docRef, {'expenses': updatedExpenses});
+    });
+  }
+
+  @override
+  Stream<List<Expense>> getExpensesForTrip(String tripId) {
+    return _firestore.collection('trips').doc(tripId).snapshots().map((
+      snapshot,
+    ) {
+      List<dynamic> expensesData = snapshot.get('expenses') ?? [];
+      return expensesData
+          .map(
+            (data) => _expenseFromMap(
+              data as Map<String, dynamic>,
+              data['id'] as String,
+            ),
+          )
+          .toList();
+    });
+  }
+
+  @override
+  Future<Expense?> getExpense(String id, String tripId) async {
+    final expensesList = await getExpensesForTrip(tripId).first;
+    try {
+      return expensesList.firstWhere((expense) => expense.id == id);
+    } catch (e) {
+      return null;
+    }
+  }
+}
